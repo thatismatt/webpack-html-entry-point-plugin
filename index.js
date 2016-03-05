38033bf795662f62c84408672f032f7c92e1f281
Matt Lee <thatismatt@gmail.com>
2016-03-05 20:18:32 +0000

    Invoke callback, even if there are no assets.

--- index.js
+++ index.js
@@ -33,52 +33,61 @@ HtmlEntryPointPlugin.prototype.apply = function (compiler) {
 
     log("Start");
 
-    // Generate a templated HTML file for each asset, (e.g. main & tests).
-    parallel(_.map(compilation.assets, function (v, asset) {
-      return function (done) {
+    var assets = _.map(compilation.assets, function (v, asset) {
+      // TODO: consider using compilation.modules instead of regex on asset.
+      var templateOptions = _.find(options.templates, function (o) { return o.test.test(asset); });
+      if (!templateOptions) {
+        throw new Error("No template options for " + asset);
+      }
+      var template = templateOptions.template;
+      return { asset, template };
+    });
 
-        // TODO: consider using compilation.modules instead of regex on asset.
-        var templateOptions = _.find(options.templates, function (o) { return o.test.test(asset); });
-        if (!templateOptions) {
-          throw new Error("No template options for " + asset);
-        }
-
-        var template = templateOptions.template;
+    if (!assets.length) {
+      log("Done - no assets");
+      callback();
+    } else {
+      // Generate a templated HTML file for each asset, (e.g. main & tests).
+      parallel(_.map(assets, function (x) {
+        var asset = x.asset;
+        var template = x.template;
         var result = path.basename(template);
 
         // Adding this line causes a "rebuild" when the template file changes.
         // *But* there is no output from the `webpack` command because the hash doesn't change.
         compilation.fileDependencies.push(path.join(compiler.context, template));
 
-        fs.readFile(template, "utf8", function (err, data) {
+        return function (done) {
+          fs.readFile(template, "utf8", function (err, data) {
 
-          if (err) {
-            if (err.code === "ENOENT" && err.path) {
-              throw new Error("Template missing at: " + err.path);
-            } else {
-              throw err;
+            if (err) {
+              if (err.code === "ENOENT" && err.path) {
+                throw new Error("Template missing at: " + err.path);
+              } else {
+                throw err;
+              }
             }
-          }
 
-          // TODO: template the raw HTML
-          var html = data.replace("{{asset}}", asset);
+            // TODO: template the raw HTML
+            var html = data.replace("{{asset}}", asset);
 
-          compilation.assets[path.join(options.output, result)] = {
-            source: function () {
-              return html;
-            },
-            size: function () {
-              return html.length;
-            }
-          };
-          log("Built: " + result);
-          done();
-        });
-      };
-    }), function () {
-      log("Done");
-      callback();
-    });
+            compilation.assets[path.join(options.output, result)] = {
+              source: function () {
+                return html;
+              },
+              size: function () {
+                return html.length;
+              }
+            };
+            log("Built: " + result);
+            done();
+          });
+        };
+      }), function () {
+        log("Done");
+        callback();
+      });
+    }
   });
 };
 
